f7700e12cf6b070a486297f9651763ca2f6c175d
Mikhail Sviridov <moishemg@vk.com>
2025-02-27 09:51:28 +0300

    поправил маршрутизацию, теперь заходит на главную страницу через / , добавил меню для телефона

--- frontend/components/NavMenu/NavMenu.vue
+++ frontend/components/NavMenu/NavMenu.vue
@@ -8,7 +8,9 @@ import "@/assets/css/navbar.css";
         <nav>
             <div class="nav--container">
                 <div class="nav--logo">
-                    <p class="logo">WildBobr</p>
+                    <a href="#">
+                        <p class="logo">WildBobr</p>
+                    </a>
                 </div>
                 <div class="nav--search">
                     <Input type="search" placeholder="Найти на WildBobr" class="nav--search__place" />
@@ -40,6 +42,40 @@ import "@/assets/css/navbar.css";
                     </a>
 
                 </div>
+            </div>
+
+            <div class="nav--container_mob">
+                <a href="" class="nav--pc_link">
+
+                    <span class="nav--pc_icon nav--mob_icon__home">
+
+                    </span>
+
+                </a>
+
+                <a href="" class="nav--pc_link">
+
+                    <span class="nav--pc_icon nav--mob_icon__category">
+
+                    </span>
+
+                </a>
+                <a href="#" class="nav--pc_link">
+                        <span class="nav--pc_icon nav--pc_icon__cart">
+
+                        </span>
+                    </a>
+
+                    <a href="#" class="nav--pc_link">
+                        <span class="nav--pc_icon nav--pc_icon__favorite">
+
+                        </span>
+                    </a>
+                <a href="#" class="nav--pc_link">
+                    <span class="nav--pc_icon nav--pc_icon__profile">
+
+                    </span>
+                </a>
             </div>
         </nav>
     </header>
